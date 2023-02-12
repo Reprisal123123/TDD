1312daa0cc2bc85c6ac4db3d703ea2a9a72e2c0a
KYK <kezy@daum.net>
2023-02-12 12:30:09 +0900

    코드 리팩토링

--- src/main/java/chap03/ExpiryDateCalculator.java
+++ src/main/java/chap03/ExpiryDateCalculator.java
@@ -1,6 +1,7 @@
 package chap03;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
 
@@ -9,23 +10,41 @@ public class ExpiryDateCalculator {
     }
 
     public LocalDate calculateExpiryDate(PayData payData) {
-        int addedMonths = 1;
+        int addedMonths = payData.getPayAmount() / 10_000;
 
+        // 첫 납부일자가 있으면
         if(payData.getFirstBillingDate() != null) {
-            // 후보 만료일 구함
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+            return expiryDateUsingFirstBillingDate(payData, addedMonths);
 
-            // 첫 납부일의 일자와 후보 만료일의 일자가 다르면
-            if(payData.getFirstBillingDate().getDayOfMonth() !=
-                candidateExp.getDayOfMonth()) {
-                // 첫 납부일의 일자를 후보 만료일의 일자로 사용
-                return candidateExp.withDayOfMonth(
-                        payData.getFirstBillingDate().getDayOfMonth());
-            }
-            if(payData.getFirstBillingDate().equals(LocalDate.of(2019, 1, 31))) {
-                return LocalDate.of(2019, 3, 31);
-            }
+        } else {
+            return payData.getBillingDate().plusMonths(addedMonths);
         }
-        return payData.getBillingDate().plusMonths(addedMonths);
+
+    }
+
+    private LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonths) {
+        // 후보 만료일 구함
+        LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+
+        // 첫 납부일자
+        final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+
+        // 첫 납부일의 일자와 후보 만료일의 일자가 다르면
+        if(dayOfFirstBilling != candidateExp.getDayOfMonth()) {
+
+            // 후보 만료일이 포함된 달의 마지막날이 첫 납부일의 일자보다 작으면
+            // 후보 만료일을 그 달의 마지막 날로 설정
+            final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+
+            if(dayLenOfCandiMon < dayOfFirstBilling) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+            }
+
+            // 첫 납부일의 일자를 후보 만료일의 일자로 사용
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+
+        } else {
+            return candidateExp;
+        }
     }
 }
